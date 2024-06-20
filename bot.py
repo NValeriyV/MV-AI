b51559c13e7da154998f69e5c7e6a953c738c773
julia558v <odobeskoulia24@gmail.com>
2024-06-20 16:31:38 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -12,26 +12,26 @@ async def start(message: telebot.types.Message):
 @bot.message_handler(commands=["help"])
 async def start_help(message: telebot.types.Message):
     main_buttons = InlineKeyboardMarkup()
-    how_generate_music = InlineKeyboardButton('1 вопрос', callback_data='var1')
-    how_generate_music1 = InlineKeyboardButton('2 вопрос', callback_data='var2')
-    how_generate_music2 = InlineKeyboardButton('3 вопрос', callback_data='var3')
-    how_generate_music3 = InlineKeyboardButton('4 вопрос', callback_data='var4')
-    main_buttons.add(how_generate_music)
+    how_generate_music = InlineKeyboardButton('1', callback_data='var1')
+    how_generate_music1 = InlineKeyboardButton('2', callback_data='var2')
+    how_generate_music2 = InlineKeyboardButton('3', callback_data='var3')
+    how_generate_music3 = InlineKeyboardButton('4', callback_data='var4')
+    main_buttons.add(how_generate_music, how_generate_music1, how_generate_music2, how_generate_music3)
     await bot.send_message(message.from_user.id, "Частые вопросы:\nКак сгенерировать песню?\nКак сгенерировать голос?\nГолос будет правдоподобным?\nМожно будет добавить свой голос?", reply_markup=main_buttons)
 
 @bot.callback_query_handler(func=lambda call: True)
-async def start_callback(call how_generate_music ):
+async def start_callback(call):
     if call.data == 'var1':
-        await bot.send_message(call.from_user.id, 'Вам нужно просто написать о чем будет песня, ключевые слова и любые другие характеристики.')
+        await bot.send_message(call.from_user.id, '1. Вам нужно просто написать о чем будет песня, ключевые слова и любые другие характеристики.')
     if call.data == 'var2':
         print('hello')
-        await bot.send_message(call.from_user.id, 'Выберите режим голоса, после чего отправьте нам текст песни.')
+        await bot.send_message(call.from_user.id, '2. Выберите режим голоса, после чего отправьте нам текст песни.')
     if call.data == 'var3':
         print('hello')
-        await bot.send_message(call.from_user.id, 'Результат может зависеть от качества источника звука и настройки параметров голоса. Я стараюсь создать натуральный звучащщий голос,который будет соответствовать вашим ожиданиям.')
+        await bot.send_message(call.from_user.id, '3. Результат может зависеть от качества источника звука и настройки параметров голоса. Я стараюсь создать натуральный звучащий голос,который будет соответствовать вашим ожиданиям.')
     if call.data == 'var4':
         print('hello')
-        await bot.send_message(call.from_user.id, 'Эта функция пока находится на разбработке.')
+        await bot.send_message(call.from_user.id, '4. Эта функция пока находится на разбработке.')
 
 async def main():
     await bot.infinity_polling()
