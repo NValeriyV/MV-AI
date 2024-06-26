fabf5e05661e2caf8f7b352dfdb7d7f3a3a55acd
julia558v <odobeskoulia24@gmail.com>
2024-06-26 13:48:01 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,28 +1,54 @@
 import asyncio
 import telebot
+import subprocess
 from telebot.async_telebot import AsyncTeleBot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
+from config import TOKEN
+from db1 import DataBase
 
-bot = AsyncTeleBot('6906669457:AAFBFgK4PUKJEu_M8tGNVBgxu_U0uskx4Zw')
+db = DataBase('test.db')
+
+bot = AsyncTeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 async def start(message: telebot.types.Message):
+    if not db.true_user_id(message.from_user.id):
+        db.register_users_id(message.from_user.id)
     await bot.send_message(message.from_user.id, 'Привет! Я MV AI. Пишу тексты песен по запросам и музыкальное сопровождение. Чтобы начать,\nвыберите режим.')
 
-@bot.message_hanler(commands=['help'])
+@bot.message_handler(commands=["help"])
 async def start_help(message: telebot.types.Message):
     main_buttons = InlineKeyboardMarkup()
-    how_generate_music = InlineKeyboardButton('1', callback_data='var1')
-    how_generate_music1 = InlineKeyboardButton('2', callback_data='var2')
-    how_generate_music2 = InlineKeyboardButton('3', callback_data='var3')
-    how_generate_music3 = InlineKeyboardButton('4', callback_data='var4')
+    how_generate_music = InlineKeyboardButton('1 вопрос', callback_data='var1')
+    how_generate_music1 = InlineKeyboardButton('2 вопрос', callback_data='var2')
+    how_generate_music2 = InlineKeyboardButton('3 вопрос', callback_data='var3')
+    how_generate_music3 = InlineKeyboardButton('4 вопрос', callback_data='var4')
     main_buttons.add(how_generate_music, how_generate_music1, how_generate_music2, how_generate_music3)
-    await bot.send_message(message.from_user.id, "Частые вопросы:\n1. Как сгенерировать песню?\n2. Как сгенерировать голос?\n3. Голос будет правдоподобным?\n4. Можно будет добавить свой голос?", reply_markup=main_buttons)
+    await bot.send_message(message.from_user.id, "Частые вопросы:\nКак сгенерировать песню?\nКак сгенерировать голос?\nГолос будет правдоподобным?\nМожно будет добавить свой голос?", reply_markup=main_buttons)
 
-@bot.message_handler
+'''@bot.message_handler()
 async def start_mess(message: telebot.types.Message):
-    await bot.send_message (message.from_user.id, "У вас не хватка средств на балансе. Пополните счет и повторите попытку!")
-    await bot.send_message (message.from_user.id,  "Дождитесь пока нейросеть обработует предыдущий запрос!")
+    if db.isDownload(message.from_user.id) == True:
+        await bot.send_message(message.from_user.id, "Ваш вопрос очень важен для нас и мы пытаемся обработать его как можно скорее.") 
+    elif db.isDownload == False:
+        await bot.send_message(message.from_user.id, "У вас не хватка средств на балансе. Пополните счет и повторите попытку!") '''
+
+@bot.message_handler()
+async def start_mess(message: telebot.types.Message):
+    if db.check_balance(db.get_balance(message.from_user.id), len(message.text), message.from_user.id):  
+        db.or_balance(message.from_user.id, len(message.text), '-')
+        print('test1') 
+        '''args_list = [
+                        'python3',
+                        'api.py',
+                        str(message.text),
+                        f'MALE',
+                        str(message.from_user.id), 
+                        str(message.from_user.id), 
+                    ]
+        subprocess.Popen(args_list)'''
+    else:
+        await bot.send_message(message.from_user.id, "У вас не хватка средств на балансе. Пополните счет и повторите попытку!")
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -34,11 +60,10 @@ async def start_callback(call):
         await bot.send_message(call.from_user.id, 'Выберите режим голоса, после чего отправьте нам текст песни.')
     if call.data == 'var3':
         print('hello')
-        await bot.send_message(call.from_user.id, 'Результат может зависеть от качества источника звука и настройки параметров голоса. Я стараюсь создать натуральный звучащий голос,который будет соответствовать вашим ожиданиям.')
+        await bot.send_message(call.from_user.id, 'Результат может зависеть от качества источника звука и настройки параметров голоса. Я стараюсь создать натуральный звучащщий голос,который будет соответствовать вашим ожиданиям.')
     if call.data == 'var4':
         print('hello')
         await bot.send_message(call.from_user.id, 'Эта функция пока находится на разбработке.')
-        
 
 async def main():
     await bot.infinity_polling()
