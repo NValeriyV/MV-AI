0e245bc41779ea402ce558b4ce735d86ba7c1e8d
jumpycrow145 <nekitrizm+music@gmail.com>
2024-06-26 14:08:52 +0300

    Add files via upload
    last update 26.06

--- db1.py
+++ db1.py
@@ -1,30 +1,33 @@
 import sqlite3 
+import time
+import datetime
+from datetime import datetime
 
 class DataBase():
     def __init__(self, file):
         self.connection = sqlite3.connect(file)
         self.cursor = self.connection.cursor()
 
-    def addbalance(self, balance, user_id):
+    def addbalance(self, balance, user_id):             # функция для добавлнгия баланса
         with self.connection:
             self.cursor.execute("UPDATE users SET balance = ? WHERE user_id = ?", (balance, user_id,))
 
-    def register_users_id(self, user_id):                                 
+    def register_users_id(self, user_id):              #регистрации_пользователя                   
         with self.connection:
             self.cursor.execute("INSERT INTO users (user_id, balance, isDownload) VALUES (?, 1000, 1)", (user_id,))
 
-    def get_balance(self, user_id):
+    def get_balance(self, user_id):                    #получить баланс
             self.cursor.execute("SELECT balance FROM users WHERE user_id = ?", (user_id,))
             result = self.cursor.fetchone()[0]
             return result
 
-    def write_balance(self, user_id, staff):
+    def write_balance(self, user_id, staff):        # стафка
         with self.connection:
             self.cursor.execute("SELECT balance FROM users WHERE user_id = ?", (user_id,))
             balance = self.cursor.fetchone()[0] + staff
             self.cursor.execute("UPDATE users SET balance = ? WHERE user_id = ?", (balance, user_id,))
 
-    def or_balance(self, user_id, sum, m):
+    def or_balance(self, user_id, sum, m):          #снятие и прибавление баланса
         with self.connection:
             if m == '+':
                 self.cursor.execute("SELECT balance FROM users WHERE user_id = ?", (user_id,))
@@ -35,11 +38,11 @@ class DataBase():
                 balance = self.cursor.fetchone()[0] - sum
                 self.cursor.execute("UPDATE users SET balance = ? WHERE user_id = ?", (balance, user_id,))          
 
-    def update_subscribe(self, user_id, podpiska):
+    def update_subscribe(self, user_id, podpiska):  #обновление подписки
         with self.connection:
             self.cursor.execute("UPDATE users SET podpiska = ? WHERE user_id = ?", (podpiska, user_id,))
         
-    def true_user_id(self, user_id):
+    def true_user_id(self, user_id):               #рабочий user_id
         with self.connection:
             self.cursor.execute("SELECT balance FROM users WHERE user_id = ?", (user_id,))
             try:
@@ -48,7 +51,7 @@ class DataBase():
             except:
                 return False
     
-    def isDownload(self, user_id):
+    def isDownload(self, user_id):              #файл загружен
         with self.connection:
             self.cursor.execute("SELECT isDownload FROM users WHERE user_id = ?", (user_id,))
             try:
@@ -57,7 +60,7 @@ class DataBase():
             except:
                 return False
             
-    def check_balance(self, balance, amount_symbol, user_id):
+    def check_balance(self, balance, amount_symbol, user_id): #проверка баланса
         with self.connection:
             if balance >= amount_symbol / 10:
                 self.cursor.execute("SELECT balance FROM users WHERE user_id = ?", (user_id,))
@@ -66,33 +69,55 @@ class DataBase():
             else:
                 return False
 
-    def set_mode(self, mode):
+    def set_mode(self, mode):   #установка мода
         with self.connection:
             self.cursor.execute("UPDATE users SET mode= ?", (mode,))
 
-    def amount_audio(self, user_id, video, s, amount_audio):
+    def amount_audio(self, user_id, video, s, amount_audio): #прибавление аудио
         with self.connection:
             if s == '+':
                 self.cursor.execute("SELECT amount_audio FROM users WHERE user_id = ?", (user_id,))
                 balance = self.cursor.fetchone()[0] + video
                 self.cursor.execute("UPDATE users SET amount_audio = ? WHERE user_id = ?", (amount_audio, user_id,))
         
-    def total_audio(self, user_id):
+    def total_audio(self, user_id):                 #всего аудио
         with self.connection:
             self.cursor.execute("SELECT amount_audio FROM users WHERE user_id = ?", (user_id))
             user_id = self.cursor.fetchone()[0]
 
-    def isEnable(self, tokens):
+    def isEnable(self, tokens):                     #включенный токен
         with self.connection:
             self.cursor.execute("UPDATE users SET isEnable WHERE tokens = ?", (tokens))  
             tokens = self.cursor.fetchone()[0] 
             return True
         
-    def change_status_token(self, token, isEnable):
+    def change_status_token(self, token, isEnable): #изменение статуса токена
         with self.connection:
             self.cursor.execute("UPDATE tokens SET isEnable = ? WHERE token = ?", (isEnable, token,))
 
-    def get_tokens_true(self):
+    def get_tokens_true(self):                      #получить токен true
         with self.connection:
             self.cursor.execute("SELECT token FROM tokens WHERE isEnable = ?", (1,))
             return [token[0] for token in self.cursor.fetchall()]
+        
+    def set_language(self, language, user_id):      #установка языка
+        with self.connection:
+            self.cursor.execute("UPDATE users FROM language = ? WHERE user_id = ?",(language,user_id,))
+
+    def read_language(self, user_id):               #чтение языка
+        with self.connection:
+            self.cursor.execute("SELECT language FROM users WHERE user_id = ? ",(user_id))
+
+    def set_time(self,user_id, date):               #установка времени
+        with self.connection:
+            self.cursor.execute("UPDATE users SET time = ? WHERE user_id = ?", (date, user_id,))
+
+    def get_time(self, time_str):                   #функция не работает 
+        time = datetime.datetime.strptime(time_str, '%H:%M:%S')
+        return time.strftime('%H-%M-%S')
+
+cl = DataBase('test.db')
+print(cl.set_time(5500790836, '26-06-24'))
+
+
+    
