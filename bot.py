2a8715dc1b3c948280d2f4ebf2d079eda012f422
julia558v <odobeskoulia24@gmail.com>
2024-06-26 16:50:57 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -26,6 +26,15 @@ async def start_help(message: telebot.types.Message):
     main_buttons.add(how_generate_music, how_generate_music1, how_generate_music2, how_generate_music3)
     await bot.send_message(message.from_user.id, "Частые вопросы:\nКак сгенерировать песню?\nКак сгенерировать голос?\nГолос будет правдоподобным?\nМожно будет добавить свой голос?", reply_markup=main_buttons)
 
+@bot.message_handler(commands=["setting"])
+async def start_setting(message: telebot.types.Message):
+    markup = InlineKeyboardMarkup()
+    btn_main = InlineKeyboardButton("Язык", callback_data='lenguage')
+    btn_main1 = InlineKeyboardButton("Пополнение баланса", callback_data='popolnenie') 
+
+    markup.add(btn_main, btn_main1)
+    await bot.send_message(message.from_user.id, 'Выберите функцию', reply_markup=markup)
+    
 '''@bot.message_handler()
 async def start_mess(message: telebot.types.Message):
     if db.isDownload(message.from_user.id) == True:
@@ -47,9 +56,11 @@ async def start_mess(message: telebot.types.Message):
                         str(message.from_user.id), 
                     ]
         subprocess.Popen(args_list)'''
-    else:
-        await bot.send_message(message.from_user.id, "У вас не хватка средств на балансе. Пополните счет и повторите попытку!")
-
+    else: 
+        markup = InlineKeyboardMarkup()
+        btn_main1= InlineKeyboardButton("Пополнение баланса", callback_data='popolnenie')
+        markup.add(btn_main1)
+        await bot.send_message(message.from_user.id, "У вас не хватка средств на балансе. Пополните счет и повторите попытку!", reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: True)
 async def start_callback(call):
@@ -64,6 +75,37 @@ async def start_callback(call):
     if call.data == 'var4':
         print('hello')
         await bot.send_message(call.from_user.id, 'Эта функция пока находится на разбработке.')
+
+    if call.data == 'popolnenie':
+        db.or_balance(call.from_user.id, 1000, '+')
+        await bot.send_message(call.from_user.id,  f'Вы пополнили свой баланс на 1000 токенов. Ваш баланс: {db.get_balance(call.from_user.id)}')
+
+    if call.data == 'lenguage':
+        markup = InlineKeyboardMarkup()
+        rus_btn = InlineKeyboardButton('RU 🇷🇺', callback_data='rus')
+        eng_btn = InlineKeyboardButton('ENG 🇬🇧', callback_data='eng')
+        fr_btn = InlineKeyboardButton('FR 🇫🇷', callback_data='fr')
+        de_btn = InlineKeyboardButton('DE 🇩🇪', callback_data='de')
+
+        markup.add(rus_btn, eng_btn, fr_btn, de_btn)
+        await bot.send_message(call.from_user.id, 'Выберите язык:', reply_markup=markup)
+
+    if call.data == 'rus':
+        await bot.send_message(call.from_user.id, 'Вы изменили язык на русский', reply_markup=markup) 
+        #написать запрос на изменение русского языка
+        db.set_language('rus', call.from_user.id)
+
+    if call.data == 'eng':
+        await bot.send_message(call.from_user.id, 'Вы изменили язык на английский', reply_markup=markup)
+        db.set_language('eng', call.from_user.id)
+        
+    if call.data == 'fr':
+        await bot.send_message(call.from_user.id, 'Вы изменили язык на французский', reply_markup=markup)
+        db.set_language('fr', call.from_user.id)
+
+    if call.data == 'de':
+        await bot.send_message(call.from_user.id, 'Вы изменили язык на немецкий', reply_markup=markup)
+        db.set_language('de', call.from_user.id)
 
 async def main():
     await bot.infinity_polling()
